5af7daf5e83d46d28498afb8687bd40d11957998
Richard Fortescue-Webb <rfwebb@gmail.com>
2014-10-31 14:43:05 +0000

    Rename deploy script

---
+++ travis_deploy_zoetrope.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Script to deploy the zoetrope widget after testing is sucessful
+
+#Make sure the script fails if any commands inside fail.
+set -e
+
+#Use the secure private key to clone the private github repo.
+ssh-agent (ssh-add ~/.ssh/id_ci_github; git clone git@github.com:ZoetropeImaging/widget.git)
+
+cd widget
+git submodule init
+git submodule update
+cd lib/jquery-zoetrope
+git checkout $TRAVIS_BRANCH
+npm install
+cd ../..
+npm install
+gulp
+http-server -p 8111 zoetrope-jquery/dist
+
+
+
