3899a5c9fb512346f64c7f66db74a6a786c11ffe
Ben Howes <ben@ben-howes.co.uk>
2014-11-25 15:29:13 +0000

    better data name transformations depending on jQuery version

--- src/zoetrope.js
+++ src/zoetrope.js
@@ -1400,9 +1400,13 @@
 	}
 	function dataPrefix(name){
 		// since pre jquery 1.6 doesnt support lower camelcase -> html 5 attr,
-		// do the conversion here.
-		var dashed = name.replace(/[A-Z]/, function(v) { return '-' + v.toLowerCase(); });
-		return ('zoe-' + dashed) || '';
+		// do the conversion here, if sub 1.6.
+		var version = $ && $().jquery.split(/\./);
+		if (!version || +(twochar(version[0])+twochar(version[1])+twochar(version[2] || '')) < 10600){
+			var dashed = name.replace(/[A-Z]/, function(v) { return '-' + v.toLowerCase(); });
+			return ('zoe-' + dashed) || '';
+		}
+		return ('zoe' + name.charAt(0).toUpperCase() + name.slice(1)) || '';
 	}
 
 	function hasTouch(){
